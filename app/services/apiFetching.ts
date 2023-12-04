52c04fefec196fa8f7a3e7bc1246e71fabd5bebe
Joseph Naval <josephnaval47@gmail.com>
2023-12-04 20:16:56 +0800

    modification

--- app/services/apiFetching.ts
+++ app/services/apiFetching.ts
@@ -2,6 +2,7 @@
 export default async function fetchApi(endpoint: string, payload: any){
     try{
         const data = await fetch(window.origin + "/api/" +endpoint, payload);
+        console.log(data);
         return data.json()
     }catch(e){
         return e
