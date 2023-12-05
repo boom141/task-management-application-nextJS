bb2c134fd6f862c4c01ca95c1f12f237f0d4092a
Joseph Naval <josephnaval47@gmail.com>
2023-12-05 23:11:19 +0800

    modification

--- app/services/apiFetching.ts
+++ app/services/apiFetching.ts
@@ -1,5 +1,6 @@
 export default async function fetchApi(endpoint: string, payload: any = {}){
     try{
+        payload.cache = 'no-store'
         payload.next = {revalidate: 3600}
         const data = await fetch(window.origin + "/api/" +endpoint, payload);
         return data.json()
