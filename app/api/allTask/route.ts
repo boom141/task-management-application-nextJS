296fbdf92edd6a8fce8e7a5b4c97ddc1184ca1f1
Joseph Naval <josephnaval47@gmail.com>
2023-12-06 00:18:51 +0800

    modification

--- app/api/allTask/route.ts
+++ app/api/allTask/route.ts
@@ -6,7 +6,11 @@ import { NextResponse } from "next/server";
 export async function GET(){
   try{
     const result = await prisma.task.findMany({orderBy:[{ id: 'desc' }]})
-    return NextResponse.json(result)
+    const res = NextResponse.json(result)
+    res.headers.set('Cache-Control', 'no-store')
+
+    return res
+
   }catch(err){
     return NextResponse.json({errorMessage:err, status:500});
   }
--- app/services/apiFetching.ts
+++ app/services/apiFetching.ts
@@ -1,4 +1,4 @@
-export const fetcher = (url:string) => fetch(url).then((res) => res.json());
+export const fetcher = (url:string) => fetch(url,{cache:'no-store'}).then((res) => res.json());
 
 export  async function fetchApi(endpoint: string, payload: any = {}){
     try{
